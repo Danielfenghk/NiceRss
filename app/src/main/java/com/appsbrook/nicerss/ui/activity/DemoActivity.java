28f8631cc7f74a66e19c8f4746c210f59e73844a
zavanton <zavanton@yandex.ru>
2018-05-18 14:15:44 +0300

    Added parsing RSS functionality.

--- app/src/main/java/com/appsbrook/nicerss/ui/activity/DemoActivity.java
+++ app/src/main/java/com/appsbrook/nicerss/ui/activity/DemoActivity.java
@@ -8,6 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.appsbrook.nicerss.R;
+import com.prof.rssparser.Article;
+import com.prof.rssparser.Parser;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import timber.log.Timber;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -22,8 +30,51 @@ public class DemoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                parseRss();
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void parseRss() {
+
+        //url of RSS feed
+        String urlString = "http://www.feedforall.com/sample.xml";
+        Parser parser = new Parser();
+        parser.execute(urlString);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+
+            @Override
+            public void onTaskCompleted(ArrayList<Article> list) {
+
+                for (Article article : list) {
+
+                    String title = article.getTitle();
+                    String author = article.getAuthor();
+                    String content = article.getContent();
+                    String description = article.getDescription();
+                    String image = article.getImage();
+                    List<String> categories = article.getCategories();
+                    Date pubDate = article.getPubDate();
+                    String link = article.getLink();
+
+                    Timber.d("Title: " + title + "\n"
+                            + "Author: " + author + "\n"
+                            + "Description: " + description + "\n"
+                            + "Image: " + image + "\n"
+                            + "Categories: " + categories + "\n"
+                            + "Publication date: " + pubDate + "\n"
+                            + "Link: " + link + "\n"
+                            + "Content: " + content + "\n");
+                }
+            }
+
+            @Override
+            public void onError() {
+                //what to do in case of error
             }
         });
     }
